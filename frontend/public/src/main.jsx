367207686f6fb8c97e7c9161db9cecca00ce8b83
CODExR <106008744+rahul-p-007@users.noreply.github.com>
2025-03-23 20:00:25 +0530

    Add files via upload

---
+++ frontend/public/src/main.jsx
@@ -0,0 +1,33 @@
+import { createContext, StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+export const Context = createContext({
+  isAuthenticated: false,
+  setIsAuthenticated: () => {},
+  user: null,
+  setUser: () => {},
+});
+
+const Appwrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  return (
+    <Context.Provider
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        user,
+        setUser,
+      }}
+    >
+      <App />
+    </Context.Provider>
+  );
+};
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Appwrapper />
+  </StrictMode>
+);
